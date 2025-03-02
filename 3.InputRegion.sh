c2da2b1fe72841a4ce9081ef5037d4204937e9e9
MANOJ <manojborra@ebsco.com>
2025-03-02 19:28:30 +0530

    Test some Args

---
+++ 3.InputRegion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# In below statements $1 can be called as argument or Param  which is passed to the script
+REGION=$1
+
+echo "Lets Get VPC Information for region $REGION..."
+aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId" -r
+VPC_COUNT=$(aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId" -r | wc -l)
+echo $VPC_COUNT
+echo ${VPC_COUNT}
+echo "${VPC_COUNT}"
+echo "$REGION has a total VPC count of ${VPC_COUNT} VPC's"
+echo "OK"
