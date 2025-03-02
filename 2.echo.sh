193b956bbc5d660252acbbdd69b3eefaf8e39a01
MANOJ <manojborra@ebsco.com>
2025-03-02 18:29:52 +0530

    Testing ECHO

---
+++ 2.echo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Lets Get VPC Information for region us-east-1..."
+aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r
+VPC_COUNT=$(aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId" -r | wc -l)
+echo $VPC_COUNT
+echo ${VPC_COUNT}
+echo "${VPC_COUNT}"
+echo "US-EAST-1 has a total VPC count of ${VPC_COUNT} VPC's"
+echo "OK"
+
+
+
+
+
+#aws ec2 describe-vpcs --region us-east-1
+#aws ec2 describe-vpcs --region us-east-1 | jq
+#aws ec2 describe-vpcs --region us-east-1 | jq ".Vpc[]"
+#aws ec2 describe-vpcs --region us-east-1 | jq ".Vpcs[].VpcId
