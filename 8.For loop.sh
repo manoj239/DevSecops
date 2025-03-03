363aa5d40b89ef1e4279fbe2255306272080ae2b
MANOJ <manojborra@ebsco.com>
2025-03-03 23:42:34 +0530

    Updating If conditions

---
+++ 8.For loop.sh
@@ -0,0 +1,14 @@
+For is used for Iterations or loops
+# type 1
+for I in {1..10}; do
+    echo $I
+done
+
+for I in {1..10..2}; do
+    echo $I
+done
+
+
+for (( I=1: I<=5; I++)); do
+    echo "welcome $c times"
+done
