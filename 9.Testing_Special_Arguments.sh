b294bb8abdf188321aa6f6299880bccd8a746a9d
MANOJ <manojborra@ebsco.com>
2025-03-03 23:58:10 +0530

    Updating If conditions

--- 9.Testing_Special_Arguments.sh
+++ 9.Testing_Special_Arguments.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+if [ $? gt 0]
 echo "You have Provided $# Arguments to this script..."
+echo "Lets Get VPC Information for region $1..."
+aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r
+VPC_COUNT=$(aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r | wc -l)
+echo "$1" has a total VPC count of ${VPC_COUNT} VPCs..."
+else
+echo "No region Arg Provided . Please provide a valid AWS Region Name .."
