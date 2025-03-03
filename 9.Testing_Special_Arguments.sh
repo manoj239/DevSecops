0be140e4b7b689c1ff52446ba56cc165e70852bb
MANOJ <manojborra@ebsco.com>
2025-03-04 00:08:30 +0530

    Adding file 4 for file 5

--- 9.Testing_Special_Arguments.sh
+++ 9.Testing_Special_Arguments.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-if [ $# -gt 0 ]
-then 
-echo "You have Provided $# Arguments to this script..."
-echo "Lets Get VPC Information for region $1..."
-aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r
-VPC_COUNT=$(aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r | wc -l)
-echo "$1" has a total VPC count of ${VPC_COUNT} VPCs..."
+if [ $# -gt 0 ]; then 
+    echo "You have Provided $# Arguments to this script..."
+    echo "Lets Get VPC Information for region $1..."
+    aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r
+    VPC_COUNT=$(aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r | wc -l)
+    echo "$1" has a total VPC count of ${VPC_COUNT} VPCs..."
 else
-echo "No region Arg Provided . Please provide a valid AWS Region Name .."
+    echo "No region Arg Provided . Please provide a valid AWS Region Name .."
 fi
