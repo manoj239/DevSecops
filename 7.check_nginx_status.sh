c1904b8369c0ef8416c5286d8cd716a0d4885464
MANOJ <manojborra@ebsco.com>
2025-03-03 23:07:36 +0530

    Updating If conditions

--- 7.check_nginx_status.sh
+++ 7.check_nginx_status.sh
@@ -8,3 +8,13 @@ else
     service nginx start
     curl http://localhost  grep -i nginx
 fi
+
+
+#!/bin/bash
+docker --version
+if  test $? -eq 0 ; then
+    echo "The Service Docker  is already running, lets validate using curl...."
+else
+    echo "Lets start validating Docker service..."
+    service docker start
+fi
