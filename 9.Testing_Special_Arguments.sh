34c0beded428049e7ab66596c989be1f79c83f13
MANOJ <manojborra@ebsco.com>
2025-03-03 23:59:17 +0530

    Updating If conditions

--- 9.Testing_Special_Arguments.sh
+++ 9.Testing_Special_Arguments.sh
@@ -7,3 +7,4 @@ VPC_COUNT=$(aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r | wc -l)
 echo "$1" has a total VPC count of ${VPC_COUNT} VPCs..."
 else
 echo "No region Arg Provided . Please provide a valid AWS Region Name .."
+fi
