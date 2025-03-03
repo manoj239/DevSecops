06985ee88c3bb83742de6265819b0675def1ab14
MANOJ <manojborra@ebsco.com>
2025-03-03 18:46:24 +0530

    Adding file 4 for file 5

--- 7.check_nginx_status.sh
+++ 7.check_nginx_status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 systemctl status nginx --no-pager > /dev/null
-if [ $? -eq 0]; then
+if [ $? -eq 0 ]; then
     echo "The Service nginx is already running, lets validate using curl...."
     curl http://localhost | grep -i nginx
 else
