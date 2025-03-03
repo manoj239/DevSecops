7247e124f8d0c195377215401ffbf05b95c5425c
MANOJ <manojborra@ebsco.com>
2025-03-03 17:00:36 +0530

    Adding file 4 for file 5

---
+++ 6.Multipositional_args.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x  #Enable Debug
+REGION=$1
+REGION=$2
+REGION=$3
+echo "$0"
+echo "$1"
+echo "$2"
+echo "$3"
+echo "Lets Get VPC Information for region $REGION1..."
+aws ec2 describe-vpcs --region $REGION1 | jq ".Vpcs[].VpcId" -r
+VPC_COUNT=$(aws ec2 describe-vpcs --region $REGION1 | jq ".Vpcs[].VpcId" -r | wc -l)
+echo "$REGION1" has a total VPC count of ${VPC_COUNT} VPCs..."
+echo "##############################################################################"
+echo "Lets Get VPC Information for region $REGION2..."
+aws ec2 describe-vpcs --region $REGION2 | jq ".Vpcs[].VpcId" -r
+VPC_COUNT=$(aws ec2 describe-vpcs --region $REGION2 | jq ".Vpcs[].VpcId" -r | wc -l)
+echo "$REGION2" has a total VPC count of ${VPC_COUNT} VPCs..."
+echo "##############################################################################"
+echo "Lets Get VPC Information for region $REGION3..."
+aws ec2 describe-vpcs --region $REGION3 | jq ".Vpcs[].VpcId" -r
+VPC_COUNT=$(aws ec2 describe-vpcs --region $REGION3 | jq ".Vpcs[].VpcId" -r | wc -l)
+echo "$REGION3" has a total VPC count of ${VPC_COUNT} VPCs..."
+echo "##############################################################################"
