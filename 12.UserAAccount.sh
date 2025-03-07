e67e80181fe2adc99407c36fc063d0c63f8b3cb8
MANOJ <manojborra@ebsco.com>
2025-03-08 02:15:39 +0530

    Operands'

--- 12.UserAAccount.sh
+++ 12.UserAAccount.sh
@@ -3,11 +3,13 @@
 #Force the user to reset the password on the first login
 
 #!/bin/bash
-read -p " Please enter the name of the user:" USERNAME
-echo $USERNAME
-EXISTING_USER=$(cat /etc/passwd | grep -i -w $USERNAME | cut -d ":" -f 1)
-if [ "${EXISTING_USER}" = "${USERNAME}" ]; then
-    echo "The Username $USERNAME already exists..Use a different one"
-else
-    echo "Lets Create a new user $USERNAME"
-fi
+while true; do
+    read -p " Please enter the name of the user:" USERNAME
+    echo $USERNAME
+    EXISTING_USER=$(cat /etc/passwd | grep -i -w $USERNAME | cut -d ":" -f 1)
+    if [ "${EXISTING_USER}" = "${USERNAME}" ]; then
+        echo "The Username $USERNAME already exists..Use a different one"
+    else
+        echo "Lets Create a new user $USERNAME"
+    fi
+done
