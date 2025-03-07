3e062a57286837ec877f22395e2ef7034b60304a
MANOJ <manojborra@ebsco.com>
2025-03-08 01:14:02 +0530

    Operands'

--- 11.Find Even-odd.sh
+++ 11.Find Even-odd.sh
@@ -15,7 +15,7 @@ done
 #!/bin/bash
 rm -rf /tmp/EVEN.log && rm -rf /tmp/ODD.log
 for I in {2..30}; do
-    if [ $(expr $I % 2) == 0 ]; then
+    if [ $(($I % 2)) == 0 ]; then
         echo "I is $I and its a EVEN number" | tee -a /tmp/TEE-EVEN.log
     else
         echo  "I is $I and its a ODD number" | tee -a /tmp/TEE-ODD.log
