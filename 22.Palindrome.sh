289738de2cae2b919f208f49223f09ae5efcf720
MANOJ <manojborra@ebsco.com>
2025-03-12 19:45:09 +0530

    Testing palindrome

--- 22.Palindrome.sh
+++ 22.Palindrome.sh
@@ -8,20 +8,14 @@
 #echo $STRING_LENGTH
 #-----------
 
+#!/bin/bash
+
 INPUT_STRING=$1
 SIZE=${#INPUT_STRING}
-STRING_LENGTH=$(expr $SIZE -1)
+STRING_LENGTH=$(expr $SIZE - 1)
 REVERSE=''
-for ((I = $STRING_LENGTH; I >= 0; I--)); do
-    REVERSE=${REVERSE}${INPUT_STRING[@]:$I:1}
-done
-if [ "${INPUT_STRING}" = "${REVERSE}" ]; then
-    echo "$INPUT_STRING Is a palindrome"
-else
-    echo "$INPUT_STRING Is Not a palindrome"
-fi
 
-for ((I = $STRING_LENGTH; I >= 0; I--)); do
+for (( I=$STRING_LENGTH; I>=0; I-- )); do
     REVERSE=${REVERSE}${INPUT_STRING:$I:1}
 done
 
