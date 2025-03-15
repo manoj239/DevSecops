3a72f005bb139c90e2fc0fad5be82fa856c75bd7
MANOJ <manojborra@ebsco.com>
2025-03-15 22:54:09 +0530

    Finding File

---
+++ 25.Rotating_logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set +x
+
+# File path for log rotation
+#f=/var/log/nginx/access.log
+read -p "Enter The File Path(Eg:/var/log/nginx/access.log): " f
+
+if [ ! -f $f ]; then
+    echo "$f does not exist!"
+    exit
+fi
+
+#touch ${f}
+MAXSIZE=$((10 * 1024))
+
+# Get the actual file size
+size=$(du -b ${f} | tr -s '\t' ' ' | cut -d ' ' -f1)
+
+# Check if file size exceeds the limit
+if [ ${size} -gt ${MAXSIZE} ]; then
+    echo Rotating!
+
+    # Create timestamp
+    timestamp=$(date +%s)
+    # Rename the log file with timestamp
+    mv ${f} ${f}.$timestamp
+    # Create a new empty log file
+    touch $f
+else
+    echo "The file '$f' size is less than 10MB. No need to rotate."
+fi
